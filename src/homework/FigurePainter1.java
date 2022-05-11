abaadf25b2de853909c57a3492438626357f1560
sevakmartirosyan92 <sevakmarti@gmail.com>
2022-05-11 12:38:46 +0400

    Figurepainter1

---
+++ src/homework/FigurePainter1.java
@@ -0,0 +1,29 @@
+package homework;
+
+public class FigurePainter1 {
+    public static void main(String[] args){
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
