8d21d4db8f87b55f15883392c3272709736196e1
sevakmartirosyan92 <sevakmarti@gmail.com>
2022-05-23 19:25:44 +0400

    chapter5

--- src/homework/arrayutil/CharArrayExample.java
+++ src/homework/arrayutil/CharArrayExample.java
@@ -2,30 +2,49 @@ package homework.arrayutil;
 
 public class CharArrayExample {
     public static void main(String[] args) {
-        /* Ունենք հետևյալ մասիվը՝char[] chars = {'j','a','v','a','l','o','v', 'e'};
-            ունենք նաև մի սինվոլ՝ char c = 'o';
-           Պետք է գրել լոգիկա, որը կհաշվի թե քանի հատ կա c-փոփոխականից մեր մասիվի մեջ, ու տպել այդ քանակը։*/
-
-         /*Ունենք հետևյալ մասիվը՝
-        char[] chars = {'j','a','v','a','l','o','v', 'e'};
-                պետք է գրել կոդ, որը կտպի մեջտեղի 2 սինվոլները։*/
-//        int middleIndex = chars.length/2;
-//        System.out.println(char[middleIndex]);
-//        System.out.println(chars[middleIndex/2 - 1]);
-
-         /*Ունենք հետևյալ մասիվը՝՝
-          char[] chars3 = {'j','a','v','a','l','o','l', 'y'};
-          պետք է տպենք true եթե մեր մասիվը վերջանում է ly-ով, եթե ոչ՝ false*/
-
-        /*Ունենք հետևյալ մասիվը՝
-        char[] bobArray = {'b','a',b','o','l','a'};
-//Տպում ենք true եթե տեքստը պարունակում է bob բառը, բայց o-ի տեղը կարող է լինել ցանկացած սինվոլ։*/
-
-        /*Ունենք հետևյալ մասիվը՝
-        char[] text = {' ',' ','b','a','r','e','v',' ',' '};
-        պետք է տպել մասիվի այն սինվոլները որոնք պռաբել չեն.*/
+//1
+        char[] chars = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e'};
+        char c = 'o';
+        int count = 0;
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == c) {
+                count++;
+            }
+        }
+        System.out.println("count = " + count);
+//2
+        char[] chars2 = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e'};
+        int middleIndex = chars2.length / 2;
+        System.out.print(chars2[middleIndex]);
+        System.out.println(chars[middleIndex / 2 - 1]);
+//3
+        char[] chars3 = {'j', 'a', 'v', 'a', 'l', 'o', 'l', 'y'};
+        char x = chars3[chars.length - 2];
+        char y = chars3[chars.length - 1];
+        if (x == 'l' && y == 'y') {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+//4
+        char[] bobArray = {'b', 'a', 'b', 'o', 'l', 'a'};
+        boolean bobResult = false;
+        for (int i = 0; i < bobArray.length; i++) {
+            if (bobArray[i] == 'b' && bobArray[i + 2] == 'b') {
+                bobResult = true;
+                break;
+            }
+        }
+        System.out.println("bobResult = " + bobResult);
+//5
+        char[] text = {' ',' ','b','a', ' ','r','e','v',' ',' '};
+        for (char c1 : text) {
+            if (c1 !=' '){
+                System.out.print(c1);
+            }
 
         }
     }
+}
 
 
