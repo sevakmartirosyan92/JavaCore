5b85e630e59cec9d92ebdcfa561a7b36d94b98d6
sevakmartirosyan92 <sevakmarti@gmail.com>
2022-05-23 20:31:45 +0400

    arraysort

--- src/homework/arrayutil/ArraySort.java
+++ src/homework/arrayutil/ArraySort.java
@@ -1,2 +1,21 @@
+package homework.arrayutil;
 
+public class ArraySort {
+    public static void main(String[] args) {
+        int temp;
+        int[] numbers = {43, 55, 5, -9, 0, 12, 5, 65};
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i; j < numbers.length; j++) {
+                if (numbers[i] < numbers[j]) {
+                    temp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < numbers.length ; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
 
