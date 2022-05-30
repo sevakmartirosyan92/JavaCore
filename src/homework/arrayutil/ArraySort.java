4db49a5a5487459f5a496c46c8397f3230e81d4d
sevakmartirosyan92 <sevakmarti@gmail.com>
2022-05-30 11:47:54 +0400

    ArraySpaceExample

--- src/homework/arrayutil/ArraySort.java
+++ src/homework/arrayutil/ArraySort.java
@@ -5,16 +5,16 @@ public class ArraySort {
         int temp;
         int[] numbers = {43, 55, 5, -9, 0, 12, 5, 65};
         for (int i = 0; i < numbers.length; i++) {
-            for (int j = i; j < numbers.length; j++) {
-                if (numbers[i] < numbers[j]) {
+            for (int j = 1; j < numbers.length; j++) {
+                if (numbers[i] < numbers[j - 1]) {
                     temp = numbers[i];
-                    numbers[i] = numbers[j];
-                    numbers[j] = temp;
+                    numbers[i] = numbers[j - 1];
+                    numbers[j - 1] = temp;
                 }
             }
         }
-        for (int i = 0; i < numbers.length ; i++) {
-            System.out.println(numbers[i]);
+        for (int x : numbers) {
+            System.out.print(x + " ");
         }
     }
 }
