cb3c5d87225e92862e3a4c6f3404a7dc442f4333
sevakmartirosyan92 <sevakmarti@gmail.com>
2022-06-19 22:26:15 +0400

    Figure

--- src/homework/dinamicarray/DinamicArray.java
+++ src/homework/dinamicarray/DinamicArray.java
@@ -3,6 +3,33 @@ package homework.dinamicarray;
 public class DinamicArray {
     int[] array = new int[10];
     int size = 0;
+    int index = 0;
+
+    //Գրել մեթոդ isEmpty անունով, որը կվերադարձնի true եթե մեր dynamicArray-ի մեջ չունենանք ոչ մի էլեմենտ.
+    // Եթե ունենք՝ false
+    public boolean isEmpty() {
+        if (index > 0 && index <= size) {
+        }
+        return false;
+    }
+
+    //Գրել մեթոդ getByIndex անունով, որը կընդունի ինդեքս, և կվերադարձնի այդ ինդեքսի տակ ընկած թիվը,
+    // եթե չկա թող վերադարձնի 0;
+    public void getByIndex(int index) {
+        if (index >= size || index < 0)
+            System.out.println("no element in this index");
+        else return;
+    }
+    //Գրել մեթոդ add(int index, int value) ,,,,,
+    public void add(int index, int value){
+        for (int i = size - 1; i >=index ; i--) {
+            array[i+1] = array[1];
+        }
+    }
+
+
+
+
 
 
     void add(int value) {
