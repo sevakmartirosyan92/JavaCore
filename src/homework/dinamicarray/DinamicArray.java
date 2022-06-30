fbf7c1e904d29daa8aa14b53a4b4b3f4c4ec6db3
sevakmartirosyan92 <sevakmarti@gmail.com>
2022-06-30 11:05:23 +0400

    dynamicarray

--- src/homework/dinamicarray/DinamicArray.java
+++ src/homework/dinamicarray/DinamicArray.java
@@ -1,15 +1,25 @@
 package homework.dinamicarray;
 
 public class DinamicArray {
-    int[] array = new int[10];
-    int size = 0;
-    int index = 0;
+    private int[] array = new int[10];
+    private int size = 0;
 
-    void add(int value) {
+    public void add(int value) {
         if (size == array.length) {
             increaseArray();
         }
         array[size++] = value;
+    }
+
+    public void add(int index, int value) {
+        if (size == array.length) {
+            increaseArray();
+        }
+        for (int i = size; i >= index; i--) {
+            array[i] = array[i - 1];
+        }
+        array[index] = value;
+        size++;
     }
 
     private void increaseArray() {
@@ -20,10 +30,49 @@ public class DinamicArray {
         array = temp;
     }
 
-    void print() {
+    public void print() {
         for (int i = 0; i < size; i++) {
             System.out.print(array[i] + " ");
 
+        }
+        System.out.println();
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int getByIndex(int index) {
+        if (index >= 0 && index < size) {
+            return array[index];
+        }
+        return 0;
+    }
+
+    public int getFirstIndexByValue(int value) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+
+    }
+
+    public void set(int index, int value) {
+        if (index >= 0 && index < size) {
+            array[index] = value;
+        } else {
+            System.out.println("invalid index");
+        }
+    }
+
+    public void delete(int index) {
+        if (index >= 0 && index < size) {
+            for (int i = index; i < size; i++) {
+                array[i] = array[i + 1];
+            }
+            size--;
         }
     }
 }
---
+++ src/homework/dinamicarray/DynamicArrayDemo.java
@@ -0,0 +1,24 @@
+package homework.dinamicarray;
+
+public class DynamicArrayDemo {
+    public static void main(String[] args) {
+        DinamicArray array = new DinamicArray();
+
+//        array.add(8);
+//        array.add(86);
+//        array.add(76);
+//        array.add(0);
+//        array.add(3);
+//        array.add(3);
+//        array.add(3);
+//        array.add(3);
+//        array.add(3);
+//        array.add(3);
+//        array.add(4);
+        array.print();
+        // array.set(1, 96);
+        //array.add(1,98);
+        //array.delete(1);
+        System.out.println(array.isEmpty());
+    }
+}
